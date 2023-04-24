3da61e41e405c5472ee0c5a31b3694bc56261953
DanieleMarino70 <danielaotitan@gmail.com>
2023-04-24 15:18:42 +0200

    first commit milestone 3

---
+++ src/App.vue
@@ -0,0 +1,21 @@
+<script>
+import AppHeader from "./components/AppHeader.vue";
+
+export default {
+  data() {
+    return {
+      title: "Hello world"
+    }
+  },
+
+  components: {
+    AppHeader,
+  },
+};
+</script>
+
+<template>
+  <AppHeader/>
+</template>
+
+<style lang="scss" scoped></style>
