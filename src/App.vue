5361217f6a84cb1252e25a76385e69c7f1571ff7
DanieleMarino70 <danielaotitan@gmail.com>
2023-04-25 19:18:17 +0200

    add: AppHeader/ProjectList/ProjectCard components

--- src/App.vue
+++ src/App.vue
@@ -1,21 +1,45 @@
 <script>
+import axios from "axios";
 import AppHeader from "./components/AppHeader.vue";
+import ProjectList from "./components/ProjectList.vue";
 
 export default {
   data() {
     return {
-      title: "Hello world"
+      title: "Hello world",
+      projects: [],
     }
   },
 
   components: {
-    AppHeader,
+    AppHeader, ProjectList
   },
+
+  methods: {
+    fetchProjects(){
+      axios.get('http://127.0.0.1:8000/api/projects').then((response) => {
+        console.log(response.data.projects);
+        this.projects = response.data.projects;
+        console.log(this.projects);
+      });
+    }
+  },
+
+  created(){
+    this.fetchProjects();
+  }
 };
 </script>
 
 <template>
   <AppHeader/>
+
+  <ProjectList :projects="projects"/>
 </template>
 
-<style lang="scss" scoped></style>
+<style lang="scss">
+li{
+  list-style: none;
+  padding: 0;
+}
+</style>
